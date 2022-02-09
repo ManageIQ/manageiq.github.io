1be639331323f2fd0001256281527e4e8807005f
Oleg Barenboim <chessbyte@gmail.com>
2022-02-09 09:35:06 -0500

    Fixed bundle install and refactored commit message

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,19 @@ git config --global user.email "noreply@manageiq.org"
 git clone --bare --branch master --depth 5 https://x-access-token:${GITHUB_TOKEN}@github.com/ManageIQ/manageiq.github.io .git
 git config core.bare false
 git add -A
-git commit -m "${COMMIT_MESSAGE}"
+
+commit_message=$(
+ruby << RUBY
+  message =
+    if ENV["PAYLOAD_PR_NUMBER"].to_s =~ /^(\d+)$/
+      "#{ENV["PAYLOAD_REPOSITORY"]}##{\$1}"
+    else
+      "#{ENV["PAYLOAD_REPOSITORY"]}@#{ENV["PAYLOAD_SHA"]} (#{ENV["PAYLOAD_REF_NAME"]})"
+    end
+
+  puts message
+RUBY
+)
+
+git commit -m "Deploy for ${commit_message}"
 git push origin master
